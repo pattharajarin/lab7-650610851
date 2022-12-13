2f608b66c156d98f5ac5054518f2cd3d59eb388b
Pattharajarin <pattharajarin_p@cmu.ac.th>
2022-12-13 16:50:35 +0700

    File lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -23,3 +23,14 @@ int adiff(int a, int b){
     }
   }
 }
+
+int main(){
+  cout << adiff(180,270);
+  cout << adiff(210,45);
+  cout << adiff(0,360);
+  cout << adiff(10,350);
+  cout << adiff(95,260);
+  cout << adiff(90,-90);
+  cout << adiff(1000,280);
+  cout << adiff(60,244);
+}
