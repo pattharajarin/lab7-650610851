b4e995f5d6e8a917a8c72e51a28366fef371b963
Pattharajarin <pattharajarin_p@cmu.ac.th>
2022-12-13 16:45:05 +0700

    File lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 
 
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int adiff(int a, int b){
+  int c, d;
+  if(a<b){
+    c=b-a;
+    d=c%360;
+    if(d>180){
+      return d=360-d;
+    }else{
+      return d;
+    }
+  }else{
+    c=a-b;
+    d=c%360;
+    if(d>180){
+      return d=360-d;
+    }else{
+      return d;
+    }
+  }
 }
